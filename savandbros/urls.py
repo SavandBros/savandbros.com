25056024250d2687f8a58a52109c7130a35cd6ce
Alireza Savand <alireza.savand@gmail.com>
2017-10-19 13:30:25 +0400

    Add static urls conf

--- savandbros/urls.py
+++ savandbros/urls.py
@@ -1,5 +1,13 @@
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     url(r'^', include('basement.urls', namespace='basement')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static('/media/', document_root=settings.MEDIA_ROOT)
