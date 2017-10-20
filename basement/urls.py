7ba4c70ec08cc109ffa929203e61b91802dc2ea2
Amir Savand <amir@savandbros.com>
2017-10-21 00:42:06 +0400

    Add contact us

--- basement/urls.py
+++ basement/urls.py
@@ -4,4 +4,5 @@ from basement import views
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^about/$', views.AboutView.as_view(), name='about'),
+    url(r'^contact/$', views.ContactView.as_view(), name='contact'),
 ]
