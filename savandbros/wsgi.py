27b253a8fbc98723b78ce00ba0b293fb1699ad79
Alireza Savand <alireza.savand@gmail.com>
2017-12-12 16:17:47 +0400

    Change to savandbros settings

--- savandbros/wsgi.py
+++ savandbros/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kloud51.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "savandbros.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
