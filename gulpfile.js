a9e3770108fb94377ef50d835bb86b45f978e050
Alireza Savand <alireza.savand@gmail.com>
2018-07-18 22:01:25 +0300

    Add minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
+var exec = require('child_process').exec;
 
 gulp.task('minify', function() {
   return gulp.src('public/**/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('public'));
+});
+
+gulp.task('build', function() {
+  exec('hugo', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+  });
+  exec('gulp minify', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+  });
 });
